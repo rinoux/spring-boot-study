631d6ebc75f1714455a59e1cadb715295460e09c
rinoux <rinoux@foxmail.com>
2017-08-08 11:44:08 +0800

    initiator changed

--- src/main/java/com/fr/demo/AppStater.java
+++ src/main/java/com/fr/demo/AppStater.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AppStater {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AppStater.class, args);
 	}
 }
